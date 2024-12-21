6fd961c3fa118abc8d030e8a23a12123141d8af0
Erik <erik.hallros@motionstudios.se>
2024-12-22 00:26:31 +0100

    Update

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ def install_requirements():
     if os.path.exists("requirements.txt"):
         subprocess.check_call([ "pip", "install", "--no-cache-dir", "-r", "requirements.txt" ])
 
-def fetch_models():
-    from huggingface_hub import login, hf_hub_download
-    login(token="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")
-    os.makedirs("/home/user/app/src/models", exist_ok=True)
-    hf_hub_download(repo_id="black-forest-labs/FLUX.1-dev", filename="ae.safetensors", local_dir="/home/user/app/src/models")
-    hf_hub_download(repo_id="comfyanonymous/flux_text_encoders", filename="t5xxl_fp8_e4m3fn.safetensors", local_dir="/home/user/app/src/models")
-    hf_hub_download(repo_id="comfyanonymous/flux_text_encoders", filename="clip_l.safetensors", local_dir="/home/user/app/src/models")
-    hf_hub_download(repo_id="Kijai/flux-fp8", filename="flux1-dev-fp8.safetensors", local_dir="/home/user/app/src/models")
+# def fetch_models():
+    # from huggingface_hub import login, hf_hub_download
+    # login(token="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")
+    # os.makedirs("/home/user/app/src/models", exist_ok=True)
+    # hf_hub_download(repo_id="black-forest-labs/FLUX.1-dev", filename="ae.safetensors", local_dir="/data/models")
+    # hf_hub_download(repo_id="comfyanonymous/flux_text_encoders", filename="t5xxl_fp8_e4m3fn.safetensors", local_dir="/data/models")
+    # hf_hub_download(repo_id="comfyanonymous/flux_text_encoders", filename="clip_l.safetensors", local_dir="/data/models")
+    # hf_hub_download(repo_id="Kijai/flux-fp8", filename="flux1-dev-fp8.safetensors", local_dir="/data/models")
 
 def fetch_libraries():
     os.makedirs("/home/user/app/src/", exist_ok=True)
@@ -30,7 +30,7 @@ class ChainInstall(install):
     def run(self):
         install_requirements()
         fetch_libraries()
-        fetch_models()
+        #fetch_models()
         install.run(self)
 
 setup(
